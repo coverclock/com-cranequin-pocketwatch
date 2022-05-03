59a23b3d7cb0d25394a8c492d1b71516005272d8
jsloan <jsloan@diag.com>
2022-05-03 10:30:27 -0600

    [1] Checkpoint.

--- Pocketwatch/libraries/Pocketwatch/Pocketwatch.cpp
+++ Pocketwatch/libraries/Pocketwatch/Pocketwatch.cpp
@@ -1,16 +1,20 @@
-/* vi: set ts=4 expandtab shiftwidth=4: */
 /*
  * Pocketwatch Implementation
+ * <https://github.com/coverclock/com-cranequin-pocketwatch>
+ *
  * Copyright 2022 Cranequin LLC, Colorado, USA.
  * <http://cranequin.com>
+ *
  * Licensed under the terms of the GNU LGPL version 2.1.
+ * <https://github.com/coverclock/com-cranequin-pocketwatch/blob/main/Pocketwatch/libraries/Pocketwatch/License-LGPL-21.pdf>
+ *
  * This is an original work of John Sloan.
  * <mailto:jsloan@diag.com>
  */
 
 #include "Pocketwatch.h"
 
-uint8_t Pocketwatch::getDays()
+uint8_t Pocketwatch::getDays() const
 {
     static const uint8_t DAYS[] = {
         31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
@@ -25,7 +29,7 @@ uint8_t Pocketwatch::getDays()
     return days;
 }
 
-void Pocketwatch::toLocalTime(int8_t offset, boolean dst)
+void Pocketwatch::toLocalTime(int8_t offset, bool dst)
 {
     offset %= 13; /* -12..12 */
     offset += dst ? 1 : 0; /* -12..13 */
@@ -65,7 +69,7 @@ void Pocketwatch::toLocalTime(int8_t offset, boolean dst)
 
 }
 
-char Pocketwatch::getTimeZone(int8_t offset, boolean dst)
+char Pocketwatch::getTimeZone(int8_t offset, bool dst)
 {
     static const char ZONE[] = {
         'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M',
--- Pocketwatch/libraries/Pocketwatch/Pocketwatch.h
+++ Pocketwatch/libraries/Pocketwatch/Pocketwatch.h
@@ -3,12 +3,20 @@
 
 /*
  * Pocketwatch Declaration
+ * <https://github.com/coverclock/com-cranequin-pocketwatch>
+ *
  * Copyright 2022 Cranequin LLC, Colorado, USA.
  * <http://cranequin.com>
+ *
  * Licensed under the terms of the GNU LGPL version 2.1.
+ * <https://github.com/coverclock/com-cranequin-pocketwatch/blob/main/Pocketwatch/libraries/Pocketwatch/License-LGPL-21.pdf>
+ *
  * This is an original work of John Sloan.
  * <mailto:jsloan@diag.com>
  */
+
+#include <stdint.h>
+#include <stdbool.h>
 
 /*
  * Define a mechanism to adjust a time in UTC to a local time.
@@ -26,21 +34,21 @@ public:
     /*
      * Given the year and month, return the number of days in the month.
      */
-    int8_t getDays();
+    int8_t getDays() const;
 
     /*
      * Given a Pocketwatch object in UTC, a signed time zone offset in hours,
      * and a flag indicating whether Daylight Saving Time is active, return the
      * object adjusted to the local time.
      */
-    void toLocalTime(int8_t offset, boolean dst);
+    void toLocalTime(int8_t offset, bool dst);
 
     /*
      * Given a signed time zone offset in hours, return a time zone name using
      * the single letter NATO nomenclature, where 'Z' for Zulu is UTC which has
      * an offset of zero.
      */
-    static char getTimeZone(int8_t offset, boolean dst);
+    static char getTimeZone(int8_t offset, bool dst);
 
 };
 
