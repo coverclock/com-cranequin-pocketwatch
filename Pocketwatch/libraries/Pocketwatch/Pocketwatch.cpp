e5850ef19896f59cfde1ad5a04432b41d84af42c
jsloan <jsloan@diag.com>
2022-05-04 09:33:49 -0600

    [1] Checkpoint after successful (so far) test.

--- Pocketwatch/libraries/Pocketwatch/Pocketwatch.cpp
+++ Pocketwatch/libraries/Pocketwatch/Pocketwatch.cpp
@@ -1,5 +1,5 @@
 /*
- * Pocketwatch Implementation
+ * Pocketwatch Implementations
  * <https://github.com/coverclock/com-cranequin-pocketwatch>
  *
  * Copyright 2022 Cranequin LLC, Colorado, USA.
@@ -8,80 +8,117 @@
  * Licensed under the terms of the GNU LGPL version 2.1.
  * <https://github.com/coverclock/com-cranequin-pocketwatch/blob/main/Pocketwatch/libraries/Pocketwatch/License-LGPL-21.pdf>
  *
- * This is an original work of John Sloan.
+ * This is a work of John Sloan.
  * <mailto:jsloan@diag.com>
  */
 
 #include "Pocketwatch.h"
 
-uint8_t Pocketwatch::getDays() const
+Debounce::Debounce(int initial)
 {
-    static const uint8_t DAYS[] = {
-        31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
-    /*   1   2   3   4   5   6   7   8   9  10  11  12   */
-    /*  Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec  */
-    };
-    uint8_t days;
-
-    days = DAYS[(this->mon - 1) % 12];
-    if ((this->mon == 2) && ((this->year % 4) == 0) && ((this->year % 400) != 0)) { days += 1; }
-
-    return days;
+  initial = !!initial;
+  this->a = initial;
+  this->b = initial;
+  this->c = initial;
+  this->r = initial;
+  this->p = initial;
 }
 
-void Pocketwatch::toLocalTime(int8_t offset, bool dst)
+int Debounce::debounce(int input)
 {
-    offset %= 13; /* -12..12 */
-    offset += dst ? 1 : 0; /* -12..13 */
-    this->hour += offset;
+  this->c = this->b;
+  this->b = this->a;
+  this->a = !!input;
+  this->p = this->r;
+  this->r = (this->p & (this->a | this->b | this->c)) | (this->a & this->b & this->c);
 
-    if (offset < 0) {
-        if (this->hour < 0) {
-            this->date -= 1;
-            if (this->date <= 0) {
-                this->mon -= 1;
-                if (this->mon <= 0) {
-                    this->year -= 1;
-                    this->mon = 12;
-                }
-                this->date = this->getDays();
-            }
-            this->hour = 23;
-            this->dow = ((this->dow + 5) % 7) + 1; /* 1..7 */
-        }
-    } else if (offset > 0) {
-        if (this->hour > 23) {
-            this->date += 1;
-            if (this->date > this->getDays()) {
-                this->mon += 1;
-                if (this->mon > 12) {
-                    this->year += 1;
-                    this->mon = 1;
-                }
-                this->date = 1;
-            }
-            this->hour = 0
-            this->dow = (this->dow % 7) + 1; /* 1..7 */
-        }
-    } else {
-        /* Do nothing. */
+  return this->r;
+}
+
+Localtime::Localtime()
+{
+  this->year = 1970;
+  this->mon = 1;
+  this->date = 1;
+  this->hour = 0;
+  this->dow = 4;
+}
+
+int8_t Localtime::getDays() const
+{
+  static const uint8_t DAYS[] = {
+    31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
+  /*  1   2   3   4   5   6   7   8   9  10  11  12   */
+  /* Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec  */
+  };
+  int8_t days;
+
+  days = DAYS[(this->mon - 1) % 12];
+  if ((this->mon == 2) && ((this->year % 4) == 0) && ((this->year % 400) != 0)) {
+    days += 1;
+  }
+
+  return days;
+}
+
+void Localtime::toLocalTime(int8_t offset, bool dst)
+{
+  offset %= 13; /* -12..12 */
+  offset += dst ? 1 : 0; /* -12..13 */
+
+  if (offset == 0) {
+    return;
+  }
+
+  this->hour += offset;
+  if (this->hour < 0) {
+    this->date -= 1;
+    if (this->date <= 0) {
+      this->mon -= 1;
+      if (this->mon <= 0) {
+        this->year -= 1;
+        this->mon += 12;
+      }
+      this->date += this->getDays();
     }
+    this->hour += 24;
+    this->dow = ((this->dow + 5) % 7) + 1; /* 1..7 */
+  } else if (this->hour >= 24) {
+    this->date += 1;
+    uint8_t days = this->getDays();
+    if (this->date > days) {
+      this->mon += 1;
+      if (this->mon > 12) {
+        this->year += 1;
+        this->mon -= 12;
+      }
+      this->date -= days;
+    }
+    this->hour -= 24;
+    this->dow = (this->dow % 7) + 1; /* 1..7 */
+  } else {
+    /* Do nothing. */
+  }
 
 }
 
-char Pocketwatch::getTimeZone(int8_t offset, bool dst)
+char Localtime::getTimeZone(int8_t offset, bool dst)
 {
-    static const char ZONE[] = {
-        'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M',
-    /*  [0]  [1]  [2]  [3]  [4]  [5]  [6]  [7]  [8]  [9]  [10] [11] [12] [13] [14] [15] [16] [17] [18] [19] [20] [21] [22] [23] [24]  */
-    /*  -12  -11  -10   -9   -8   -7   -6   -5   -4   -3   -2   -1   0    +1   +2   +3   +4   +5   +6   +7   +8   +9  +10  +11  +12   */
-    };
-    char zone;
+  static const char ZONE[] = {
+    'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M',
+/*  [0]  [1]  [2]  [3]  [4]  [5]  [6]  [7]  [8]  [9]  [10] [11] [12] [13] [14] [15] [16] [17] [18] [19] [20] [21] [22] [23] [24]  */
+/*  -12  -11  -10   -9   -8   -7   -6   -5   -4   -3   -2   -1    0   +1   +2   +3   +4   +5   +6   +7   +8   +9  +10  +11  +12   */
+  };
+  char zone;
 
-    offset %= 13; /* -12..12 */
-    offset += 12; /*  0..24  */
-    zone = ZONE[offset]; /* 'Y'..'M' */
-    if (dst) { zone += ' '; } /* 'y'..'m' */
+  offset %= 13; /* -12..12 */
+  offset += 12; /*  0..24  */
 
-    return zone;
+  zone = ZONE[offset]; /* 'Y'..'M' */
+
+  if (dst) {
+    zone += ' '; /* 'y'..'m' */
+  }
+
+  return zone;
 }
--- Pocketwatch/libraries/Pocketwatch/Pocketwatch.h
+++ Pocketwatch/libraries/Pocketwatch/Pocketwatch.h
@@ -2,7 +2,7 @@
 #define _H_COM_CRANEQUIN_POCKETWATCH_
 
 /*
- * Pocketwatch Declaration
+ * Pocketwatch Declarations
  * <https://github.com/coverclock/com-cranequin-pocketwatch>
  *
  * Copyright 2022 Cranequin LLC, Colorado, USA.
@@ -11,7 +11,7 @@
  * Licensed under the terms of the GNU LGPL version 2.1.
  * <https://github.com/coverclock/com-cranequin-pocketwatch/blob/main/Pocketwatch/libraries/Pocketwatch/License-LGPL-21.pdf>
  *
- * This is an original work of John Sloan.
+ * This is a work of John Sloan.
  * <mailto:jsloan@diag.com>
  */
 
@@ -19,36 +19,87 @@
 #include <stdbool.h>
 
 /*
- * Define a mechanism to adjust a time in UTC to a local time.
+ * Debounce a digital input.
+ *
+ * REFERENCES
+ *
+ * Jack Gannsle, "A Guide to Debouncing, or How To Debounce a Contact in Two
+ * Easy Pages", 2008-06, http://www.ganssle.com/debouncing.htm
+ *
+ * Jack Gannsle, "A Guide to Debouncing - Part 2, or How To Debounce a Contact
+ * in Two Easy Pages", 2008-06, http://www.ganssle.com/debouncing-pt2.htm
+ *
+ * Jack Ganssle, "My Favorite Software Debouncers", 2004-06-16,
+ * http://www.embedded.com/electronics-blogs/break-points/4024981/My-favorite-software-debouncers
+ *
+ * Dr. Marty, "The Best Switch Debounce Routine Ever", 2009-05-20,
+ * http://drmarty.blogspot.com/2009/05/best-switch-debounce-routine-ever.html
+ *
+ * Jack Ganssle, "A Guide to Debouncing", 2008-06,
+ * http://www.eng.utah.edu/~cs5780/debouncing.pdf
  */
-class Pocketwatch {
+class Debounce {
 
 public:
 
-    int8_t hour; /* 0..23 */
-    int8_t date; /* 1..31 */
-    int8_t mon;  /* 1..12 */
-    int8_t year;
-    int8_t dow;  /* 1..7 */
+  uint8_t a : 1;
+  uint8_t b : 1;
+  uint8_t c : 1;
+  uint8_t r : 1;
+  uint8_t p : 1;
 
-    /*
-     * Given the year and month, return the number of days in the month.
-     */
-    int8_t getDays() const;
+  /*
+   * Constructor.
+   */
+  Debounce(int initial);
 
-    /*
-     * Given a Pocketwatch object in UTC, a signed time zone offset in hours,
-     * and a flag indicating whether Daylight Saving Time is active, return the
-     * object adjusted to the local time.
-     */
-    void toLocalTime(int8_t offset, bool dst);
+  /*
+   * Given a digital input, debounce it and return the output.
+   */
+  int debounce(int input);
 
-    /*
-     * Given a signed time zone offset in hours, return a time zone name using
-     * the single letter NATO nomenclature, where 'Z' for Zulu is UTC which has
-     * an offset of zero.
-     */
-    static char getTimeZone(int8_t offset, bool dst);
+};
+
+/*
+ * Define a mechanism to adjust a time in UTC to a local time given a
+ * signed integer hour time zone offset in the range -12..12. This algorithm
+ * does not support half-hour offsets (which exist).
+ */
+class Localtime {
+
+public:
+
+  int16_t year;
+  int8_t mon;  /* 1..12 */
+  int8_t date; /* 1..31 */
+  int8_t hour; /* 0..23 */
+  int8_t dow;  /* 1..7 */
+
+  /*
+   * Constructor.
+   */
+  Localtime();
+
+  /*
+   * Given a Pocketwatch object, return the number of days in the month
+   * for that year.
+   */
+  int8_t getDays() const;
+
+  /*
+   * Given a Pocketwatch object in UTC, a signed time zone offset in hours,
+   * and a flag indicating whether Daylight Saving Time is active, return the
+   * object adjusted to the local time.
+   */
+  void toLocalTime(int8_t offset, bool dst);
+
+  /*
+   * Given a signed time zone offset in hours, return a time zone name using
+   * the single letter NATO nomenclature, where 'Z' for Zulu is UTC which has
+   * an offset of zero. If Daylight Saving Time is enabled for this time zone,
+   * convert the time zone name to lower case to make it obvious.
+   */
+  static char getTimeZone(int8_t offset, bool dst);
 
 };
 
